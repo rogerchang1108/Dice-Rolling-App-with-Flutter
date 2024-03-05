3fb8e8e07c699738e1fed6d48fa69238c15b2af1
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-05 22:48:08 +0800

    Add Features: Score-count & replay

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -15,26 +15,59 @@ class DiceRoller extends StatefulWidget {
 
 class _DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll = 2;
+  int rollCount = 0;
+  List<int> diceRolls = List.filled(5, 0);
 
   void rollDice() {
     setState(() {
-      currentDiceRoll = randomizer.nextInt(6) + 1;
-      // print('${currentDiceRoll}');
+      if (rollCount < 5) {
+        currentDiceRoll = randomizer.nextInt(6) + 1;
+        // print('${currentDiceRoll}');
+        diceRolls[rollCount++] = currentDiceRoll;
+      }
+    });
+  }
+  
+  void resetGame() {
+    setState(() {
+      currentDiceRoll = 2;
+      diceRolls = List.filled(5, 0);
+      rollCount = 0;
     });
   }
 
   @override
   Widget build(context) {
+    int totalScore = diceRolls.fold(0, (previousValue, element) => previousValue + element);
+    String message = '$totalScore/20';
+    String buttonText = rollCount < 5 ? 'Roll Dice' : 'Play again';
+
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        const Text(
+          'Your Score:',
+          style: TextStyle(
+            fontSize: 24, 
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          message,
+          style: const TextStyle(
+            fontSize: 32, 
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 20),
         Image.asset(
           'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(height: 20),
         TextButton(
-          onPressed: rollDice,
+          onPressed: rollCount < 5 ? rollDice : resetGame,
           style: TextButton.styleFrom(
             // padding: const EdgeInsets.only(
             //   top: 20,
@@ -44,7 +77,7 @@ class _DiceRollerState extends State<DiceRoller> {
               fontSize: 28,
             ),
           ),
-          child: const StyledText('Roll Dice'),
+          child: StyledText(buttonText),
         )
       ],
     );
